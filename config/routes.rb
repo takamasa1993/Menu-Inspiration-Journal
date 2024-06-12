7414fb005c847eee10428592492158be5d0cd61d
EC2 Default User <165451724+takamasa1993@users.noreply.github.com>
2024-06-12 07:28:43 +0000

    Add-routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,37 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'homes#top'
+
+  get 'about', to: 'homes#about', as: 'about'
+
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+
+  resources :users, only: [:show, :edit, :update, :destroy] do
+    collection do
+      get 'my_page', to: 'users#show'
+      get 'information/edit', to: 'users#edit'
+      patch 'information', to: 'users#update'
+      get 'unsubscribe', to: 'users#unsubscribe'
+      patch 'withdraw', to: 'users#withdraw'
+    end
+    member do
+      post 'follow', to: 'follows#create'
+      delete 'unfollow', to: 'follows#destroy'
+    end
+  end
+
+  resources :posts do
+    resource :likes, only: [:create, :destroy]
+    resources :comments, only: [:create, :destroy]
+  end
+
+  resources :searches, only: [:index]
+
+  namespace :admin do
+    resources :users, only: [:index, :show, :edit, :update, :destroy]
+    resources :genres, except: [:show]
+    resources :ingredients, only: [:index, :show, :update]
+  end
 end
