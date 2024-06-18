13ec15425c6ef138fd5ef74a55ee9b42c2db06f3
EC2 Default User <165451724+takamasa1993@users.noreply.github.com>
2024-06-18 11:27:30 +0000

    webpacker

--- babel.config.js
+++ babel.config.js
@@ -7,8 +7,11 @@ module.exports = function(api) {
 
   if (!validEnv.includes(currentEnv)) {
     throw new Error(
-      'Please specify a valid `NODE_ENV` or `BABEL_ENV` environment variables. Valid values are "development", ' +
-      '"test", and "production". Instead, received: ' + JSON.stringify(currentEnv) + '.'
+      'Please specify a valid `NODE_ENV` or ' +
+        '`BABEL_ENV` environment variables. Valid values are "development", ' +
+        '"test", and "production". Instead, received: ' +
+        JSON.stringify(currentEnv) +
+        '.'
     )
   }
 
@@ -65,9 +68,7 @@ module.exports = function(api) {
       [
         '@babel/plugin-transform-runtime',
         {
-          helpers: false,
-          regenerator: true,
-          corejs: false
+          helpers: false
         }
       ],
       [
