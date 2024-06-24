3d3d2cdf1d22c1ef69b988727f81bf125ffd7bfc
EC2 Default User <165451724+takamasa1993@users.noreply.github.com>
2024-06-24 15:35:09 +0000

    Add-searchs

--- app/controllers/user/searches_controller.rb
+++ app/controllers/user/searches_controller.rb
@@ -1,4 +1,9 @@
 class User::SearchesController < ApplicationController
   def index
+    if params[:search].blank?
+      @posts = Post.none
+    else
+      @posts = Post.where('menu_title LIKE ?', "%#{params[:search]}%").where(is_public: true)
+    end
   end
 end
