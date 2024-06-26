a26bca0b8e756b347aa5cb18cddec7b1115e1e71
EC2 Default User <165451724+takamasa1993@users.noreply.github.com>
2024-06-26 10:23:33 +0000

    change-migrate

--- db/migrate/20240625164404_add_default_status_to_new_ingredient_proposals.rb
+++ db/migrate/20240625164404_add_default_status_to_new_ingredient_proposals.rb
@@ -1,5 +1,5 @@
 class AddDefaultStatusToNewIngredientProposals < ActiveRecord::Migration[6.1]
   def change
-    change_column_default :new_ingredient_proposals, :status, 'pending'
+    # change_column_default :new_ingredient_proposals, :status, 'pending'
   end
 end
