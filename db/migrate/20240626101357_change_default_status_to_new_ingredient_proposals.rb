9997481c60ffda5674bd2b349d73de3edb1096c2
EC2 Default User <165451724+takamasa1993@users.noreply.github.com>
2024-06-26 10:27:24 +0000

    cange-migrate

--- db/migrate/20240626101357_change_default_status_to_new_ingredient_proposals.rb
+++ db/migrate/20240626101357_change_default_status_to_new_ingredient_proposals.rb
@@ -1,5 +1,9 @@
 class ChangeDefaultStatusToNewIngredientProposals < ActiveRecord::Migration[6.1]
-  def change
-    change_column_default :new_ingredient_proposals, :status, from: nil, to: "pending"
+  def up
+    execute "ALTER TABLE new_ingredient_proposals ALTER COLUMN status SET DEFAULT 'pending'"
+  end
+
+  def down
+    execute "ALTER TABLE new_ingredient_proposals ALTER COLUMN status DROP DEFAULT"
   end
 end
