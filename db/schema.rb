8d358deee13bb208fc8297218481cc025f75c417
EC2 Default User <165451724+takamasa1993@users.noreply.github.com>
2024-07-03 14:48:01 +0000

    change-header-sidebar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_06_26_101357) do
+ActiveRecord::Schema.define(version: 2024_06_26_103331) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -86,7 +86,7 @@ ActiveRecord::Schema.define(version: 2024_06_26_101357) do
   create_table "new_ingredient_proposals", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "name", null: false
-    t.integer "status", null: false
+    t.string "status", default: "pending", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_new_ingredient_proposals_on_user_id"
