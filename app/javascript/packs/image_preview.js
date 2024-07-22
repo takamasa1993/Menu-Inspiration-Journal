a1540f51e5594f8b4cea4874809a87a504ec3738
EC2 Default User <165451724+takamasa1993@users.noreply.github.com>
2024-07-22 04:11:07 +0000

    change-posts_new_link

--- app/javascript/packs/image_preview.js
+++ app/javascript/packs/image_preview.js
@@ -1,17 +1,25 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   const imageInput = document.getElementById('post_image');
   const imagePreview = document.getElementById('image_preview');
 
-  if (imageInput && imagePreview) {
-    imageInput.addEventListener('change', event => {
-      const file = event.target.files[0];
+  if (imageInput) {
+    imageInput.addEventListener('change', function() {
+      const file = this.files[0];
       if (file) {
         const reader = new FileReader();
-        reader.onload = e => {
-          imagePreview.src = e.target.result;
-        };
+        reader.onload = function(event) {
+          imagePreview.src = event.target.result;
+        }
         reader.readAsDataURL(file);
       }
     });
+  }
+});
+
+// Turbolinksを無効化する処理を追加
+document.addEventListener("turbolinks:before-visit", function(event) {
+  const form = document.querySelector('form');
+  if (form) {
+    form.setAttribute('data-turbolinks', 'false');
   }
 });
