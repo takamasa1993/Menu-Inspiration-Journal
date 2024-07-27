65a634335555ae0fbe67f8c54d229b2412d76270
EC2 Default User <165451724+takamasa1993@users.noreply.github.com>
2024-07-27 09:31:11 +0000

    modify-views/top

--- app/controllers/user/homes_controller.rb
+++ app/controllers/user/homes_controller.rb
@@ -2,7 +2,7 @@ class User::HomesController < ApplicationController
   def top
     @genres = Genre.all
     @latest_posts_by_genre = Genre.joins(:posts).distinct.map do |genre|
-      [genre.name, genre.posts.order(created_at: :desc).limit(9)]
+      [genre.name, genre.posts.where(is_public: true).order(created_at: :desc).limit(9)]
     end.to_h
   end
 end
